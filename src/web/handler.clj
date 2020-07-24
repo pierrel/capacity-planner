a62515c6ebcd7f611a8b9bede620a27194721083
Pierre Larochelle <pierrebombay@gmail.com>
2020-07-24 07:48:07 -0700

    HTML response

--- src/web/handler.clj
+++ src/web/handler.clj
@@ -5,7 +5,8 @@
         ring.adapter.jetty)
   (:require [capacity.config :as config]
             [capacity.report :as report]
-            [capacity.utils :as utils])
+            [capacity.utils :as utils]
+            [hiccup.core :as h])
   (:import [capacity.core Eng Project]))
 
 (defn summarize [filename]
@@ -15,14 +16,29 @@
     (map (partial apply report/full-summary)
          (apply utils/group-interleave res-w-teams))))
 
+(defn render-summary-item
+  [heading lst]
+  [:div
+   [:h2 heading]
+   [:ul
+    (map #(vector :li %) lst)]])
+
+(defn render-summary [summary]
+  (map #(render-summary-item (-> % first name) (last %)) summary))
+
+(defn section
+  [form]
+  [:div form])
+
 (defn with-response [resp]
-  (-> (response resp)
+  (-> (response (h/html resp))
       (content-type "text/html")
       (status 200)))
 
 (defn routes [{uri :uri}]
   (case uri
-    "/" (with-response (summarize "config-fy21.edn"))
+    "/" (with-response (map (comp section render-summary)
+                            (summarize "config-fy21.edn")))
     (-> (response "Page not found")
         (status 404))))
 
