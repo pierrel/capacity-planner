9e8bfacaafb473bf6734a38b8fa8f3192c310672
Pierre Larochelle <pierrebombay@gmail.com>
2020-10-11 17:34:36 -0700

    Adding testing script

---
+++ utils/test.clj
@@ -0,0 +1,21 @@
+(ns test
+  (:require  [clojure.test :as t]))
+
+(defn test-file? [file]
+  (and (.isFile file)
+       (re-matches #".*\.clj[^.]*$" (.getPath file))))
+
+(defn load-return-failures [files]
+  (doseq [file files]
+    (try
+      (load-file (.getPath file))
+      (catch Exception e (println "Coult not load file" (.getPath file) "," e)))))
+
+(defn -main [& args]
+  (let [dir-name "test"
+        directory (clojure.java.io/file dir-name)
+        files (filter test-file?
+                      (file-seq directory))]
+    (load-return-failures files)
+    (t/run-all-tests)))
+
