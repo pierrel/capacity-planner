16bf5b4f5740e128da1d6be699bf0ae07fa23e3f
Pierre Larochelle <pierrebombay@gmail.com>
2020-11-21 12:27:33 -0800

    Using new, simplified core in runner
    No need for reporter anymore

--- src/capacity/runner.clj
+++ src/capacity/runner.clj
@@ -1,51 +1,65 @@
 (ns capacity.runner
   (:require [capacity.config :as config]
             [capacity.utils :as utils]
-            [capacity.report :as report]
             [clojure.string :as s])
   (:use [capacity.core]
         [clojure.pprint])
   (:import [capacity.core Eng Project]))
 
+(defn agg-completion
+  "Takes 2 efforts and returns the total percent completion."
+  [total current]
+  (let [[agg-total agg-current] (map #(apply + (vals %))
+                                     [total current])
+        total-rec (- agg-total agg-current)]
+    (/ total-rec agg-total)))
 
-(defn report-str
-  [iteration rmap]
-  (let [all (concat (:completed rmap) (:progressed rmap))]
+(defn id-comparison-check
+  [& all]
+  (let [ids (map id all)]
+    (if (not (apply = ids))
+      (throw (format "Object ids must match to report on them. Comparing %s"
+                     (s/join ", " ids))))))
+
+(defn completion-str
+  "Takes 2 projects and returns a string representing their completion."
+  [original-project current-project]
+  (id-comparison-check original-project current-project)
+  (format "%s (%.0f%%)" ;; TODO figure out how not to show decimal places
+          (id original-project)
+          (* 100 (float
+                  (apply agg-completion
+                         (map :effort
+                              [original-project current-project]))))))
+
+(defn report-change
+  [iteration original-backlog current-backlog]
+  (let [all (map (partial apply completion-str)
+                 (utils/group-interleave original-backlog
+                                         current-backlog))]
     (format "Iteration %d: %s"
             iteration
             (s/join ", " all))))
 
-(defn report
-  [iter iter-backlog backlog-summary iter-team team-summary]
-  (report-str iter (report/full-summary iter-backlog
-                                        backlog-summary
-                                        iter-team
-                                        team-summary)))
+(defn report-changes
+  [backlogs]
+  (let [original-backlog (first backlogs)
+        after-backlogs (rest backlogs)
+        changes-strs (map (partial apply report-change)
+                         (utils/group-interleave (range)
+                                                 (repeat original-backlog)
+                                                 after-backlogs))
+        final-str (format (if (every? exhausted? (last backlogs))
+                            "Backlog complete after %d iterations."
+                            "Backlog incomplete after %d iteration.")
+                          (count after-backlogs))]
+    (format "%s\n\n%s"
+            (s/join "\n" changes-strs)
+            final-str)))
 (defn run
   [filename]
-  (let [[backlog iterations] (config/to-models filename)
-        [remaining-backlog
-         backlogs
-         backlog-summaries
-         team-summaries] (work-backlog-entirely backlog iterations)]
-    [remaining-backlog
-     backlogs
-     backlog-summaries
-     iterations
-     team-summaries]))
-
-(defn report-iterations
-  [backlogs
-   backlog-summaries
-   iterations
-   team-summaries]
-  (format "Iterations:\n%s"
-          (s/join "\n" (map #(apply report %)
-                            (utils/group-interleave (map inc (range))
-                                                    backlogs
-                                                    backlog-summaries
-                                                    iterations
-                                                    team-summaries)))))
+  (let [[backlog iterations] (config/to-models filename)]
+    (first (work-backlog-entirely backlog iterations))))
 
 (defn report-backlog
   [backlog]
@@ -53,17 +67,12 @@
           (s/join "\n" (map :name backlog))))
 
 (defn run-and-report [filename]
-  (let [[rem-backlog
-         backlogs
-         backlog-summaries
-         iterations
-         team-summaries] (run filename)]
+  (let [backlogs (run filename)]
     (format "%s\n\n%s"
             (report-backlog (first backlogs))
-            (report-iterations backlogs
-                               backlog-summaries
-                               iterations
-                               team-summaries))))
+            (report-changes backlogs))))
+
+(run-and-report "config.edn")
 
 (defn -main [& args]
   (let [file (or (first args) "config.edn")]
